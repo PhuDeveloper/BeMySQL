19e0367832e623e46daa38173a19c74d2fb44b96
vvpdevfe <phuvv@miczone.asia>
2023-06-04 22:18:37 +0700

    setup database

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root2",
+  password: "123456",
+  database: "test",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!!!");
+});
+
+app.get("/data", function (req, res) {
+  const sql = "SELECT * FROM user";
+
+  con.query(sql, function (err, results) {
+    if (err) throw err;
+    res.send(results.id);
+    console.log("resull", results[0].id);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
